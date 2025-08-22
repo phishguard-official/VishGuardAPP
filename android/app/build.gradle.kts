988c2e070f999f8cb1f8db59b3ed80190f300008
phishguard-official <minecraft.sajmonko@gmail.com>
2025-08-22 11:31:51 +0200

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.call_guard_app"
+    namespace = "org.vosk.flutter"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
